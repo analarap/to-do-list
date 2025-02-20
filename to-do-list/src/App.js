6518606d69f1c1d93685245afec780495b5ce57e
analarap <analarapupo@gmail.com>
2025-02-20 16:37:43 -0300

    feat: forms and inicial logic implemented

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -11,6 +11,22 @@ function App() {
   const [todos, setTodos] = useState([])
   const [loading, setLoading] = useState(false)
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done: false
+    }
+
+    console.log(todo)
+
+    setTitle("")
+    setTime("")
+  }
+
   return (
     <div className="App">
       <div className="todo-header">
@@ -18,11 +34,37 @@ function App() {
       </div>
 
       <div className="form-todo">
-        <p>Forms</p>
+        <h2>Insert your next task: </h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="title">What you going to do?</label>
+            <input
+              type="text"
+              name="title"
+              placeholder="Task Title"
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ""}
+              required 
+            /> 
+          </div>
+          <div className="form-control">
+            <label htmlFor="time">Duration:</label>
+            <input
+              type="text"
+              name="time"
+              placeholder="Estimated Time (Hours)"
+              onChange={(e) => setTime(e.target.value)}
+              value={time || ""}
+              required 
+            /> 
+          </div>
+          <input type="submit" value="Create Task"></input>
+        </form>
       </div>
       
       <div className="list-todo">
-        <p>List</p>
+        <h2>Task List: </h2>
+        {todos.length === 0 && <p>No task available</p>}
       </div>
     </div>
   );
